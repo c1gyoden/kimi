84b59188f64353e665f9a1293760d97a344b7552
c1gyoden <c1gyoden@vasvari.org>
2024-04-19 11:15:20 +0200

    2. feladat

---
+++ Kimi.py
@@ -0,0 +1,19 @@
+class Kimi:
+    def __init__(self, datum, nagydij, hely, befkor, pont, konstruktor, celba, hatrany, hiba):
+        self.datum = datum
+        self.nagydij = nagydij
+        self.hely = hely
+        self. befkor = befkor
+        self.pont = pont
+        self.konstruktor = konstruktor
+        self.celba = celba
+        self.hatrany = hatrany
+        self.hiba = hiba
+
+fajl = open('kimi.csv', 'rt', encoding='utf-8')
+fajl.readline()
+
+eredmenyek = []
+for sor in fajl:
+    sor = sor.strip().split(';')
+    eredmenyek.append(Kimi(sor[0], sor[1], sor[2], sor[3], sor[4], sor[5], sor[6], sor[7], sor[8]))
